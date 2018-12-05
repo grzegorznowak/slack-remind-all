5e677c5e721a07786f396b124f563d16e368a311
Grzegorz Nowak <strange3studio@gmail.com>
2018-12-05 17:16:57 +0100

    provisioning molecule tests passing

--- provision/molecule/default/tests/test_default.py
+++ provision/molecule/default/tests/test_default.py
@@ -11,15 +11,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '3000'
 ])
 def test_ports(host, port):
-    assert host.socket("tcp://127.0.0.1:{}".format(port)).is_listening
+    assert host.socket("tcp://0.0.0.0:{}".format(port)).is_listening
 
 
 @pytest.mark.parametrize('port', [
     '3000'
 ])
 def test_up(host, port):
+
     cmd = host.run(
         'curl --write-out %{http_code} --silent ' +
-        '--output /dev/null http://127.0.0.1:{}'.format(port))
+        '--output /dev/null http://{}:{}/slack/slash'.format('0.0.0.0', port))
 
-    assert cmd.stdout == '403'
+    assert cmd.stdout == '405'
